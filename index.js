a9c22a052b8ee0e96423a8e9073912bb34bee2f7
Hieu <xuanhieu0012@gmail.com>
2021-07-22 11:41:48 -0500

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(name, event){
+    const thankYouCard =[];
+    for (let i = 0; i < name.length; i++){
+        let message = `Thank you, ${name[i]}, for the wonderful ${event} gift!`
+        thankYouCard.push(message)
+       
+    };
+    return thankYouCard;
+}
+
+function countDown(num){
+    
+    while (num > -1){
+        console.log(num);
+        num--;
+    }
+}
